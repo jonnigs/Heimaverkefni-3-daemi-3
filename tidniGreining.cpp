debd8eb96c5145d93b0d57f3ccb4684b43bcea6c
jonnigs <jonnigs@gmail.com>
2017-01-31 12:00:48 +0000

    Byrjunin

--- tidniGreining.cpp
+++ tidniGreining.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+string mostFrequentWords(string text, int k) {
+    string unordered_set;
+    int lengd = text.size();
+
+
+
+
+    return unordered_set;
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
